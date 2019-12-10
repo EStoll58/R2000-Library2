fd93b2184ae06e964b20c01fd6b0ba54a86192c7
Estoll58 <estoll@us.pepperl-fuchs.com>
2019-12-10 13:22:53 -0500

    Add project files.

---
+++ R2000 Library/project.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+public class Project
+{
+
+    /********************************************
+    Distance Data is in Var.measurmentdata[]
+    Angular Data is in Var.angulardata[]
+
+    ********************************************/
+    public void project()
+    {
+        Console.WriteLine("Starting your code here");
+        
+
+
+
+
+    }
+    
+}
